a5251ade74debc79119dc261bffbeaac94bf73f1
Matt Rubin <opensource@mattrubin.me>
2014-03-16 23:04:50 -0700

    New Project

---
+++ PaperPlane/PPPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPPAppDelegate.h
+//  PaperPlane
+//
+//  Created by Matt Rubin on 3/16/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
